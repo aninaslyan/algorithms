0305ab45d5f610c586d57987ad7ffd6b6ab8985a
reactive <ani.naslyan83@gmail.com>
2019-06-19 12:46:27 +0400

    Cipher implementation, Simple Replacement algorithm

---
+++ cipher/simpleReplacement.js
@@ -0,0 +1,28 @@
+function simpleReplacement(plainText, key) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let newAlphabet = [...alphabet];
+    let coded = "", index="", newIndex="", final = [];
+
+    let unique = [...new Set(plainText)];
+
+    alphabet.forEach((value) => {
+        unique.forEach(val => {
+            if(value.includes(val)){
+                index = alphabet.indexOf(value);
+                alphabet.splice(index, 1);
+            }
+        });
+    });
+    coded = unique.concat(alphabet);
+    console.log(coded);
+
+    key = key.split("");
+
+    console.log(key);
+    key.forEach(v => {
+        newIndex = newAlphabet.findIndex(k => k===v);
+        final.push(coded[newIndex]);
+    });
+
+    return final;
+}
