4568189daee249d972a77c3c49334222c6044b59
aninaslyan <ani.naslyan@gmail.com>
2019-08-29 12:57:19 +0400

    removing sorting from Binary search logic

--- search/binary.js
+++ search/binary.js
@@ -1,21 +1,16 @@
 function binarySearch(array, targetValue) {
-    let sortedArray = [...array];
-    sortedArray.sort((a, b) => a - b);
-
-    console.log(sortedArray, targetValue);
-
     let min = 0;
-    let max = sortedArray.length - 1;
+    let max = array.length - 1;
     let guess = 0;
     let totalGuesses = 0;
 
     while (max >= min) {
         totalGuesses++;
         guess = Math.floor((min + max) / 2);
-        if (targetValue === sortedArray[guess]) {
+        if (targetValue === array[guess]) {
             console.log(totalGuesses, "totalGuesses");
             return guess;
-        } else if (sortedArray[guess] < targetValue) {
+        } else if (array[guess] < targetValue) {
             min = guess + 1;
         } else {
             max = guess - 1;
--- search/linear.js
+++ search/linear.js
@@ -2,7 +2,6 @@ function linearSearch(array, targetValue) {
     let guess = -1;
     let totalGuesses = 0;
 
-    console.log(array, targetValue);
     array.forEach((val, index) => {
         totalGuesses++;
         if (val === targetValue) {
