9da06d545ae1442b79677dd1952a88f16fa21776
Ani Naslyan <ani.naslyan@gmail.com>
2022-12-27 14:47:42 +0400

    Implementation of Bubble sort algorithm.

---
+++ sort/bubble.js
@@ -0,0 +1,22 @@
+function bubbleSort(array) {
+    let sortedArray = [...array];
+
+    for (let i = 0; i < sortedArray.length; i++) {
+        let isSwapped = false; // Optimisation 2
+        for (let j = 0; j < sortedArray.length - i - 1; j++) { // Optimisation 1
+            if (sortedArray[j + 1] < sortedArray[j]) {
+                isSwapped = true;
+                [sortedArray[j], sortedArray[j + 1]] = [sortedArray[j + 1], sortedArray[j]];
+            }
+        }
+        if (!isSwapped) {
+            return sortedArray;
+        }
+    }
+    return sortedArray;
+}
+
+// 1 - on each iteration of the outer for loop, the largest element in the array "bubbles" to the end,
+// so there is no need to compare it with other elements on subsequent iterations.
+// 2 - isSwapped flag to track whether any swaps were made during the iteration. If no swaps were made,
+// it means that the array is already sorted and the function can return the sorted array early
