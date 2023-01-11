7f6c0fcfa3368c9a3587b48e719cb85e6c58527d
Ani Naslyan <ani.naslyan@gmail.com>
2023-01-11 18:13:05 +0400

    Some improvements.

--- sort/selection.js
+++ sort/selection.js
@@ -1,13 +1,11 @@
 // Repeatedly finding the minimum element from the array and placing it at the beginning
 
 function indexOfMinimum(array, index) {
-    let minValue = array[index];
     let minIndex = index;
 
     for (let i = minIndex + 1; i < array.length; i++) {
-        if (array[i] < minValue) {
+        if (array[i] < array[minIndex]) {
             minIndex = i;
-            minValue = array[i];
         }
     }
     return minIndex;
@@ -19,7 +17,7 @@ function selectionSort(array) {
 
     for (let i = 0; i < sortedArray.length - 1; i++) { // Optimisation 1
         minIndex = indexOfMinimum(sortedArray, i);
-        if (minIndex !== i) { // if they are equal, no need to swap as no difference will occur
+        if (minIndex !== i) { // if the indexes are equal, no need to swap as no difference will occur
             swap(sortedArray, i, minIndex);
         }
     }
@@ -27,4 +25,4 @@ function selectionSort(array) {
     return sortedArray;
 }
 
-// 1: (n - 1), because the last element will already be in the end after (n - 1)th iteration
+// 1: (n - 1), because the last element will already be in the end after (n - 1)th iteration (the same as in bubble sort Optimisation1)
--- sort/utils.js
+++ sort/utils.js
@@ -1,3 +1,19 @@
 function swap(array, firstIndex, secondIndex) {
     [array[firstIndex], array[secondIndex]] = [array[secondIndex], array[firstIndex]];
 }
+
+// Recursively finding the minimum number
+function findMinimumNumber(array) {
+    if (array.length === 1) {
+        return array[0];
+    }
+
+    const number1 = findMinimumNumber(array.slice(0, array.length/2)); // Math.floor function will be used to round the float number in array.slice() method under the hood
+    const number2 = findMinimumNumber(array.slice(array.length/2, array.length)); // Math.floor function will be used to round the float number in array.slice() method under the hood
+
+    if (number1 < number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
